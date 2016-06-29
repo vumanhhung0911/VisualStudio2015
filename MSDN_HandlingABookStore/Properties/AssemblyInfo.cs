bed94d68c4767656c129c0ecf483816424ebf8eb
Hùng Vũ <vumanhhung0911@gmail.com>
2016-06-29 10:53:16 +0700

    Added new projects

---
+++ MSDN_HandlingABookStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MSDN_HandlingABookStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSDN_HandlingABookStore")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6abf8a1-1c8e-4079-a98e-673b961dc947")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSDN_UsingDelegates/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSDN_UsingDelegates
+{
+    public class MethodClass
+    {
+        public void Method1(string message) {
+            Console.WriteLine("Inside MethodClass.Method1");
+            Console.WriteLine(message);
+        }
+        public void Method2(string message) {
+            Console.WriteLine("Inside MethodClass.Method2");
+            Console.WriteLine(message);
+        }
+
+    }
+    class Program
+    {
+        public delegate void Del(string message);
+
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void MethodWithCallback(int par1, int par2, Del callback)
+        {
+            callback("The number is: " + (par1 + par2).ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            Del handler = DelegateMethod;
+            handler("Hello World");
+
+            Del handler2 = new Del(DelegateMethod);
+            handler2("I'm a machine designer");
+
+            MethodWithCallback(1, 2, handler);
+
+            MethodClass obj = new MethodClass();
+            Del d1 = obj.Method1;
+            Del d2 = obj.Method2;
+            Del d3 = DelegateMethod;
+
+            Del allMethodsDelegate = d1 + d2;
+            allMethodsDelegate += d3;
+
+            d1(allMethodsDelegate.GetInvocationList().GetLength(0).ToString());
+
+            allMethodsDelegate("Hello again");
+
+            // remove Method1
+            allMethodsDelegate -= d1;
+
+            //Copy allMethodDelegate while removing d2
+            Del oneMethodDelegate = allMethodsDelegate - d2;
+            oneMethodDelegate("Copy allMethodDelegate while removing d2");
+
+            // Instantiate Del by using an anonymous method.
+            Del del4 = delegate (string name)
+            { Console.WriteLine("Notification received for: {0}", name); };
+            del4("Anonymous method");
+
+            // Inistantiate Del by using a lambda expression.
+            Del del5 = name => { Console.WriteLine("notification received for: {0}", name); };
+            del5("Lambda expression");
+            Console.Read();
+        }
+    }
+}
